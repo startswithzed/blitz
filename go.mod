61f35b5066fb0a86c0e73b1fdd162eece57e235a
Zaid <iamzaidsheikh@gmail.com>
2023-06-25 11:19:52 +0530

    feat: add a continuously updating line graph

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/startswithzed/web-ruckus
 go 1.19
 
 require (
+	github.com/gizak/termui/v3 v3.1.0
 	github.com/shirou/gopsutil/v3 v3.23.5
 	github.com/spf13/cobra v1.7.0
 )
@@ -11,7 +12,11 @@ require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,52 @@
 package main
 
 import (
-	"github.com/startswithzed/web-ruckus/cmd"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"log"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	rootCmd := cmd.GetRootCmd()
-	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	n := 500
+
+	rps := make([][]float64, 1)
+	rps[0] = make([]float64, n)
+
+	ticker := time.NewTicker(200 * time.Millisecond)
+	i := 0
+
+	p0 := widgets.NewPlot()
+	p0.Title = "Response Time"
+	p0.Data = rps
+	p0.SetRect(0, 0, 100, 25)
+	p0.AxesColor = ui.ColorWhite
+	p0.LineColors[0] = ui.ColorGreen
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				rps[0][i] = float64(rand.Intn(20))
+				i++
+				ui.Clear()
+				ui.Render(p0)
+			}
+		}
+	}()
+
+	uiEvents := ui.PollEvents()
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			return
+		}
 	}
 }
